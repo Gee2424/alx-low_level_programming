ed172d0326aa1b4959fd037d2df237a4712c9523
Gee2424 <gkm18686@gmail.com>
2023-07-07 12:59:52 -0700

    debugged task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include "main.h"
 
+/* 
+*@argc count of arguments
+*@argv array of characters
+*Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[]) {
-    /* This line is added to explicitly ignore the unused parameter warning */
+    
     (void)argc;
     printf("%s\n", argv[0]);
     return 0;
 }
+
