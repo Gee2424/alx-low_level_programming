69789e1ef5e038d66b54da43a7241613cafb8241
Gee242 <88376078+Gee2424@users.noreply.github.com>
2023-07-12 02:39:55 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,47 +2,46 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: argument count
- * @av: argument vector
- *
- * Return: pointer to new space in memory or null
- */
+* argstostr - concatenates all the arguments of your program
+* @ac: argument count
+* @av: argument vector
+*
+* Return: pointer to new space in memory or null
+*/
 char *argstostr(int ac, char **av)
 {
-    char *strDup;
-    int i, j, k, size;
+char *strDup;
+int i, j, k, size;
 
-    if (ac == 0 || av == NULL)
-        return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-    /* count the number of chars in each string */
-    for (i = 0, size = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++, size++)
-            ;
-        size++;
-    }
-    size++;
+/* count the number of chars in each string */
+for (i = 0, size = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, size++);
+size++;
+}
+size++;
 
-    /* allocate memory for total number of chars and
-     * new line for each argument
-     */
-    strDup = malloc(sizeof(char) * size);
-    if (strDup == NULL)
-        return (NULL);
+/* allocate memory for total number of chars and
+* new line for each argument
+*/
+strDup = malloc(sizeof(char) * size);
+if (strDup == NULL)
+return (NULL);
 
-    /* append each arg to new string */
-    for (i = 0, k = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++, k++)
-        {
-            strDup[k] = av[i][j];
-        }
-        strDup[k] = '\n';
-        k++;
-    }
-    strDup[k] = '\0';
+/* append each arg to new string */
+for (i = 0, k = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++, k++)
+{
+strDup[k] = av[i][j];
+}
+strDup[k] = '\n';
+k++;
+}
+strDup[k] = '\0';
 
-    return (strDup);
+return (strDup);
 }
