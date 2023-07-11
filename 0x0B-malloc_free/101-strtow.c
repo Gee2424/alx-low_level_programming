a2f563018cfc47f50538a64f3cdf199af0186b4c
Gee242 <88376078+Gee2424@users.noreply.github.com>
2023-07-12 02:44:29 +0300

    Update 101-strtow.c to betty style

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,46 +2,45 @@
 #include <stdlib.h>
 
 /**
- * strtow - splits a string into words
- * @str: the string to split
- *
- * Return: pointer to an array of words
- */
+* strtow - splits a string into words
+* @str: the string to split
+*
+* Return: pointer to an array of words
+*/
 char **strtow(char *str)
 {
-    char **words;
-    int i, j, k, h, w_count, c_count;
+char **words;
+int i, j, k, h, w_count, c_count;
 
-    if (str == NULL || str[0] == '\0')
-        return (NULL);
+if (str == NULL || str[0] == '\0')
+return (NULL);
 
-    for (w_count = i = 0; str[i]; i++)
-        if ((str[i] != ' ' && str[i + 1] == ' ') || (str[i] != ' ' && str[i + 1] == '\0'))
-            w_count++;
+for (w_count = i = 0; str[i]; i++)
+if ((str[i] != ' ' && str[i + 1] == ' ') || (str[i] != ' ' && str[i + 1] == '\0'))
+w_count++;
 
-    words = malloc((w_count + 1) * sizeof(char *));
-    if (words == NULL)
-        return (NULL);
+words = malloc((w_count + 1) * sizeof(char *));
+if (words == NULL)
+return (NULL);
 
-    for (i = j = 0; i < w_count; i++)
-    {
-        while (str[j] == ' ')
-            j++;
-        for (c_count = 0; str[j + c_count] != ' ' && str[j + c_count]; c_count++)
-            ;
-        words[i] = malloc((c_count + 1) * sizeof(char));
-        if (words[i] == NULL)
-        {
-            for (k = 0; k < i; k++)
-                free(words[k]);
-            free(words);
-            return (NULL);
-        }
-        for (h = 0; h < c_count; h++)
-            words[i][h] = str[j + h];
-        words[i][h] = '\0';
-        j += c_count;
-    }
-    words[i] = NULL;
-    return (words);
+for (i = j = 0; i < w_count; i++)
+{
+while (str[j] == ' ')
+j++;
+for (c_count = 0; str[j + c_count] != ' ' && str[j + c_count]; c_count++);
+words[i] = malloc((c_count + 1) * sizeof(char));
+if (words[i] == NULL)
+{
+for (k = 0; k < i; k++)
+free(words[k]);
+free(words);
+return (NULL);
+}
+for (h = 0; h < c_count; h++)
+words[i][h] = str[j + h];
+words[i][h] = '\0';
+j += c_count;
+}
+words[i] = NULL;
+return (words);
 }
