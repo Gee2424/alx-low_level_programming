18d558114555b0d96b4cf68c49c38692bf54f4a3
Gee242 <88376078+Gee2424@users.noreply.github.com>
2023-07-12 02:37:53 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory,
- *           which contains a copy of the string given as a parameter
- * @str: the string to duplicate
- *
- * Return: a pointer to the duplicated string, or NULL if it fails
- */
+* _strdup - returns a pointer to a newly allocated space in memory,
+*   which contains a copy of the string given as a parameter
+* @str: the string to duplicate
+*
+* Return: a pointer to the duplicated string, or NULL if it fails
+*/
 char *_strdup(char *str)
 {
-    char *dup;
-    unsigned int i, len = 0;
+char *dup;
+unsigned int i, len = 0;
 
-    if (str == NULL) /* validate str input */
-        return (NULL);
+if (str == NULL) /* validate str input */
+return (NULL);
 
-    while (str[len]) /* get length of str */
-        len++;
+while (str[len]) /* get length of str */
+len++;
 
-    dup = malloc(sizeof(char) * (len + 1)); /* allocate memory for duplicate */
+dup = malloc(sizeof(char) * (len + 1)); /* allocate memory for duplicate */
 
-    if (dup == NULL) /* validate memory allocation */
-        return (NULL);
+if (dup == NULL) /* validate memory allocation */
+return (NULL);
 
-    for (i = 0; i < len; i++) /* copy the string */
-        dup[i] = str[i];
+for (i = 0; i < len; i++) /* copy the string */
+dup[i] = str[i];
 
-    dup[len] = '\0'; /* null terminate the string */
+dup[len] = '\0'; /* null terminate the string */
 
-    return (dup);
+return (dup);
 }
