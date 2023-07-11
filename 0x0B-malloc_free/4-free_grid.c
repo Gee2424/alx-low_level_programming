cafed6745aefe791e022a0e09c9e8ea5375ddf42
Gee242 <88376078+Gee2424@users.noreply.github.com>
2023-07-12 02:54:46 +0300

    Update 4-free_grid.c update to betty style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid
- * @grid: the 2D grid
- * @height: the height of the grid
- *
- * Return: Nothing.
- */
+* free_grid - frees a 2 dimensional grid previously created by alloc_grid
+* @grid: the 2D grid
+* @height: the height of the grid
+*
+* Return: Nothing.
+*/
 void free_grid(int **grid, int height)
 {
-    int i;
+int i;
 
-    if (grid == NULL || height <= 0)
-        return;
+if (grid == NULL || height <= 0)
+return;
 
-    for (i = 0; i < height; i++)
-        free(grid[i]);
+for (i = 0; i < height; i++)
+free(grid[i]);
 
-    free(grid);
+free(grid);
 }
 
