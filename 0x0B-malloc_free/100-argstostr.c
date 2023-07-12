525e70f7431ff93c162164fe754d44ee23898faf
Gee242 <88376078+Gee2424@users.noreply.github.com>
2023-07-12 03:15:23 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,8 +19,7 @@ return (NULL);
 /* count the number of chars in each string */
 for (i = 0, size = 0; i < ac; i++)
 {
-for (
-j = 0;
+for (j = 0;
 av[i][j] != '\0';
 j++, size++);
 size++;
@@ -28,7 +27,6 @@ size++;
 size++;
 
 /**
-*
 *allocate memory for total number of chars and
 * new line for each argument
 */
