fe6aa364d98c4f5c5c289c36efc11c2e09f81b39
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2024-03-26 03:34:25 +0300

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: pointer to memory area to be filled
- * @b: char to copy
- * @n: the number of times to copy variable b
- *
- * Return: pointer to the memory area s
- */
+* *_memset - fills memory with a constant byte
+* @s: pointer to memory area to be filled
+* @b: char to copy
+* @n: the number of times to copy variable b
+*
+* Return: pointer to the memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
 
-	return (s);
+return (s);
 }
 
