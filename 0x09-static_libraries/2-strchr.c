de8c981de4aec654b09e8e6020f312ce21758d8d
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2024-03-26 03:37:13 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,28 +2,27 @@
 #include <stdio.h>
 
 /**
- * *_strchr - locates a character in a string
- * @s: searching string
- * @c: char values to find
- *
- * Return: a pointer to the first occurrence of the character
- * value c
- */
+* *_strchr - locates a character in a string
+* @s: searching string
+* @c: char values to find
+*
+* Return: a pointer to the first occurrence of the character
+* value c
+*/
 char *_strchr(char *s, char c)
 {
-		int a;
+int a;
 
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
+while (1)
+{
+a = *s++;
+if (a == c)
+{
+return (s - 1);
 }
-
+if (a == 0)
+{
+return (NULL);
+}
+}
+}
